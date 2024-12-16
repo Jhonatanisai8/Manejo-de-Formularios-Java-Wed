e8bb0e1d40c0dfaf6d9f3cacabc44e722658bdd2
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-12-16 15:21:57 -0500

    procesando elementos select y checkbox en el servlet

--- src/main/java/com/isai/webapp/form/servlets/FormServlet.java
+++ src/main/java/com/isai/webapp/form/servlets/FormServlet.java
@@ -2,11 +2,10 @@ package com.isai.webapp.form.servlets;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.*;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
 
 @WebServlet("/registro")
 public class FormServlet extends HttpServlet {
@@ -17,6 +16,9 @@ public class FormServlet extends HttpServlet {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         String email = request.getParameter("email");
+        String pais = request.getParameter("pais");
+        String[] lenguajes = request.getParameterValues("lenguajes");
+        String[] roles = request.getParameterValues("roles");
 
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
@@ -29,8 +31,20 @@ public class FormServlet extends HttpServlet {
             out.println("   <h1>Resultado del formulario.</h1>");
             out.println("   <ul>");
             out.println("       <li> Username: " + username + "</li>");
-            out.println("<li> Password: " + password + "</li>");
+            out.println("       <li> Password: " + password + "</li>");
             out.println("       <li> Email:   " + email + "</li>");
+            out.println("       <li> Pais:   " + pais + "</li>");
+            out.print("         <li>Lenguajes: <ul>");
+            Arrays.asList(lenguajes).forEach(lenguaje -> {
+                out.println("       <li> Lenguaje:   " + lenguaje + "</li>");
+            });
+            out.print("         </ul></li>");
+            
+            out.print("         <li>Roles: <ul>");
+            Arrays.asList(roles).forEach(rol -> {
+                out.println("       <li> Rol:   " + rol + "</li>");
+            });
+            out.print("         </ul></li>");
             out.println("   </ul>");
             out.println("</body>");
             out.println("</html>");
